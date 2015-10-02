7e32454a68feb11f40fab8cc7c56ea410c1005ba
chris carrington <carringtron@gmail.com>
2015-10-02 22:39:17 +0000

    fixed typo in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
     # form where user can file out thier own profile
-    @user = User.find( params[user_id] )
+    @user = User.find( params[:user_id] )
+    @variable = params [:hello]
     @profile = @user.build_profile
+    end
 end
